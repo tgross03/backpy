9de58eed087d34155c6649162be7a922891d94a3
tgross03 <tom.gross@udo.edu>
2025-06-10 19:57:00 +0200

    Change backup.py

--- backpy/core/backup.py
+++ backpy/core/backup.py
@@ -1,13 +1,28 @@
 import uuid
 from pathlib import Path
 
+from backpy import VariableLibrary
+
 
 class Backup:
-    def __init__(self):
+    def __init__(self, name: str):
+        self._uuid: uuid.UUID = uuid.uuid4()
+        self._name: str = name
+        self._dir: Path = Path(VariableLibrary()["backup_directory"]) / str(self._uuid)
+
+    def restore_state(self, uuid: uuid.UUID):
+        raise NotImplementedError("This method is abstract and has to be overriden!")
+
+    def create_state(self):
         None
 
 
-class FileBackup:
+class BackupState:
+    def __init__(self, backup: Backup):
+        self._backup: Backup = backup
+
+
+class FileBackup(Backup):
     def __init__(self, path: str, exclude_paths: list[str] | None = None):
         self._uuid: uuid.UUID = uuid.uuid4()
         self._origin_path: Path = Path(path)
