e534ad207685758ae5463eea42d1d86e845ee0a0
tgross03 <tom.gross@udo.edu>
2026-02-27 23:15:42 +0100

    feat: Add config tests

--- backpy/core/config/__init__.py
+++ backpy/core/config/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from backpy.core.config.configuration import TOMLConfiguration
+from backpy.core.config.configuration import TOMLConfiguration, MissingKeyPolicy
 from backpy.core.config.variables import VariableLibrary
 
-__all__ = ["TOMLConfiguration", "VariableLibrary"]
+__all__ = ["TOMLConfiguration", "MissingKeyPolicy", "VariableLibrary"]
